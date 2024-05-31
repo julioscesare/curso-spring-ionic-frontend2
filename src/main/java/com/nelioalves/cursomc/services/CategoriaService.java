dc153a36662d5ffb6811770a710bfb39d17d59bc
Julio <julioscesare@gmail.com>
2024-05-31 14:14:44 -0300

    Atualizando Categoria com PUT, outros pequenos ajustes

--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -16,7 +16,7 @@ public class CategoriaService {
 	@Autowired 
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 		
 		Optional<Categoria> obj = repo.findById(id);
 				
@@ -24,12 +24,32 @@ public class CategoriaService {
 	
 	}
 	
-	
+	/**
+	 * Metodo utilizado para inserir uma Categoria nova
+	 * @param obj
+	 * @return a categoria nova após ser inserida na base
+	 */
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
 	
 	
+	/**
+	 * Método utilizado para atualizar uma Categoria existente. 
+	 * @param obj
+	 * @return
+	 */
+	public Categoria update(Categoria obj) {
+		
+		//aqui nós chamamos o método find para garantir que o id exista antes de retorná-lo. Caso ele não exista, o método find
+		//lançará uma exceção.
+		find(obj.getId());
+		
+		//Perceba que o metodo "save" tanto serve para Inserir uma categoria nova quanto para editar uma Categoria existente.
+		//O que diferencia o que ele deve executar é justamente o id. Caso o id exista, o metodo altera; caso o id não exista, o metodo
+		//insere uma Categoria nova.
+		return repo.save(obj);
+	}
 		
 }
